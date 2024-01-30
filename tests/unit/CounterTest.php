2e813626c8053af6e3f6fc242d932bda0cefb4db
Thiago Barros <thiagobarros95@gmail.com>
2024-01-30 09:01:15 -0300

    test: ajusts names and paths

--- tests/unit/CounterTest.php
+++ tests/unit/CounterTest.php
@@ -2,21 +2,25 @@
 
 use ThiagoBarros\FileCounter\Counter;
 
-it('should return the number of files .php into tests/folder dir', function (): void {
+it('should return the number of files php into tests/assets/counter-test/folder dir', function (): void {
     expect(Counter::number_of_files('tests/assets/counter-test/folder', '.php'))->toBe(1);
 });
 
-it('should return the number of files .php into tests/folder dir too', function (): void {
+it('should return the number of files .php into tests/assets/counter-test/folder dir', function (): void {
     expect(Counter::number_of_files('tests/assets/counter-test/folder', 'php'))->toBe(1);
 });
 
 it('should return the number of files searching for another random extension', function (): void {
+    mkdir('tests/assets/counter-test/empty-folder', 0755);
+
     expect(
         Counter::number_of_files(
-            'tests/assets/counter-test/folder',
+            'tests/assets/counter-test/empty-folder',
             array_rand(['txt', 'doc', 'pdf', 'jpg', 'png'], 1)
         )
     )->toBe(0);
+
+    rmdir('tests/assets/counter-test/empty-folder');
 });
 
 it('should throw invalid argument exception with message: Invalid path given', function (): void {
