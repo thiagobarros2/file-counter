77367c20f97343a5da2be75f6669f130adc1d686
Thiago Barros <thiagobarros95@gmail.com>
2024-01-30 08:51:38 -0300

    test: white better tests for assets

--- tests/assets/AssetsDirTest.php
+++ tests/assets/AssetsDirTest.php
@@ -1,25 +1,25 @@
 <?php
 
-it('should return true asserting /tests/assets/counter-test/folderphp is dir', function (): void {
-    mkdir('tests/assets/counter-test/folderphp', 0755);
-
-    expect(is_dir('tests/assets/counter-test/folderphp'))->toBetrue();
-
-    rmdir('tests/assets/counter-test/folderphp');
+it('should return true asserting /tests/assets/counter-test/folder/file.php is file', function (): void {
+    expect(is_file('tests/assets/counter-test/folder/file.php'))->toBeTrue();
 });
 
-it('should return true asserting /tests/assets/counter-test/folder.php is dir', function (): void {
-    mkdir('tests/assets/counter-test/folder.php', 0755);
-
-    expect(is_dir('tests/assets/counter-test/folder.php'))->toBetrue();
-
-    rmdir('tests/assets/counter-test/folder.php');
+it('should return true asserting /tests/assets/lister-test/folder/file.php is file', function (): void {
+    expect(is_file('tests/assets/lister-test/folder/file.php'))->toBeTrue();
 });
 
-it('should return false asserting /tests/assets/counter-test/folder/file.php is dir', function (): void {
-    expect(is_dir('tests/assets/counter-test/folder/file.php'))->toBeFalse();
+it('should return true asserting /tests/assets/lister-test/folder/a.r is file', function (): void {
+    expect(is_file('tests/assets/lister-test/folder/a.r'))->toBeTrue();
 });
 
-it('should return false asserting /tests/assets/lister-test/folder/file.php is dir', function (): void {
-    expect(is_dir('tests/assets/lister-test/folder/file.php'))->toBeFalse();
+it('should return true asserting /tests/assets/lister-test/folder/b.cs is file', function (): void {
+    expect(is_file('tests/assets/lister-test/folder/b.cs'))->toBeTrue();
+});
+
+it('should return true asserting /tests/assets/lister-test/folder/c.c is file', function (): void {
+    expect(is_file('tests/assets/lister-test/folder/c.c'))->toBeTrue();
+});
+
+it('should return true asserting /tests/assets/lister-test/folder/d.js is file', function (): void {
+    expect(is_file('tests/assets/lister-test/folder/d.js'))->toBeTrue();
 });
